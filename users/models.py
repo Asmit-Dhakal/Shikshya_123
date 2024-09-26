58208e1dbb93cc385b794d62efd57f496388099b
Asmit Dhakal <107795383+Asmit-Dhakal@users.noreply.github.com>
2024-09-26 14:42:52 +0545

    push

--- users/models.py
+++ users/models.py
@@ -1,12 +1,15 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
     is_teacher = models.BooleanField(default=False)
     is_student = models.BooleanField(default=False)
     address = models.CharField(max_length=255, blank=True, null=True)
     country = models.CharField(max_length=100, blank=True, null=True)
     phone_number = models.CharField(max_length=20, blank=True, null=True)
+    total_booked_price = models.FloatField(default=0.0)  # New field to store total price
+
 
     class Meta:
         constraints = [
@@ -16,3 +19,5 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
